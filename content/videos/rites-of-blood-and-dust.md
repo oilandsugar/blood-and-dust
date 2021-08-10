7e5b445065ce4c8d0006d46da4c8b2d425777ef5
ritualdust <lizbeth.porier.m@gmail.com>
2021-08-10 17:50:24 -0400

    update video page

--- content/videos/rites-of-blood-and-dust.md
+++ content/videos/rites-of-blood-and-dust.md
@@ -2,6 +2,7 @@
 title: Rites of Blood and Dust
 date: "2020-07-15"
 cover: "/img/dark-arts-cover.png"
-description: ""
-video: "https://youtube.com/embed/LxpAx_4iA5o"
+description: "You look tired. I don’t suppose you’ve heard it, have you? The voice? Calling you through the trees, calling you to come and rest? That sweet voice—it has a way of getting into your head. But it wants to devour you, you know. The whole forest wants to devour you. It’s devouring you from the inside, right now. You’re no different than the screaming shadows you see outside at night. You’re all heading the same way, straight into its jaws. Me? I’m hungry—can’t you tell?"
+video: "LxpAx_4iA5o"
+ratio: "hd"
 ---
--- themes/bloodanddust/static/js/script.js
+++ themes/bloodanddust/static/js/script.js
@@ -59,5 +59,46 @@ jQuery(function($) {
       playPause.addEventListener("click", togglePlayPause)
       audioPlayer.addEventListener('timeupdate', showProgress);
     }
+
+    // video player
+    let lights = true;
+
+    const lightsOut = () => {
+      let descs = document.getElementsByClassName("desc");
+      let nav = document.getElementById('main-nav');
+      let title = document.getElementById('video-title');
+      for(var counter = 0; counter < descs.length; counter++){
+        descs[counter].style.opacity = 0.2;
+      }
+      nav.style.opacity = 0.2;
+      title.style.opacity = 0.2;
+    }
+
+    const lightsOn = () => {
+      var descs = document.getElementsByClassName("desc");
+      let nav = document.getElementById('main-nav');
+      let title = document.getElementById('video-title');
+      for(var counter = 0; counter < descs.length; counter++){
+        descs[counter].style.opacity = 1;
+      }
+      nav.style.opacity = 1;
+      title.style.opacity = 1;
+    }
+
+    const toggleLights = () => {
+      if(lights) {
+        lights = false;
+        lightsOut();
+      } else {
+        lights = true;
+        lightsOn();
+      }
+    }
+
+    var videos = document.getElementsByClassName("video-container");
+    for(var counter = 0; counter < videos.length; counter++){
+      videos[counter].addEventListener("click", toggleLights);
+    }
+
   });
 });
